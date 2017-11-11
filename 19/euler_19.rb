17c2629968dcf1c8bebf8143ddc63602950a6e64
Joseph Browning <joseph.browning@macys.com>
2017-11-11 10:30:26 -0500

    solve 19 with OO

--- 19/euler_19.rb
+++ 19/euler_19.rb
@@ -1,42 +1,65 @@
 # https://projecteuler.net/problem=19
 
-# Year
-class Year
-  def leap_year?(year)
-    return false if (year % 100).zero? && !(year % 400).zero? # but not on a century unless it is divisible by 400
-    return true if (year % 4).zero? # A leap year occurs on any year evenly divisible by 4
-    false
-  end
-end
-
-# Month
-class Month
-
-end
-
-# Day
-class Day
+def leap_year?(year)
+  return false if (year % 100).zero? && !(year % 400).zero? # but not on a century unless it is divisible by 400
+  return true if (year % 4).zero? # A leap year occurs on any year evenly divisible by 4
+  false
 end
 
 # Date
 class Date
+  attr_reader :year, :month, :day, :dayofweek
+
   def initialize(year, month, day)
     @year = year
     @month = month
     @day = day
+    @dayofweek = 1
+  end
+
+  def next_dayofweek
+    if @dayofweek == 7
+      @dayofweek = 1
+    else
+      @dayofweek += 1
+    end
   end
 
   def next_day
-    if @month == 11 && @day == 31
+    next_dayofweek
+    if @month == 12 && @day == 31
       @year += 1
-      @month = 0
-      @day = 0
-    elsif @month == 1
-      if
+      @month = 1
+      @day = 1
+    elsif @month == 2 && leap_year?(@year) && @day == 29
       @month += 1
-      @day = 0
+      @day = 1
+    elsif @month == 2 && !leap_year?(@year) && @day == 28
+      @month += 1
+      @day = 1
+    elsif [1, 3, 5, 7, 8, 10].include?(@month) && @day == 31
+      @month += 1
+      @day = 1
+    elsif [4, 6, 9, 11].include?(@month) && @day == 30
+      @month += 1
+      @day = 1
     else
       @day += 1
     end
   end
 end
+
+cur_date = Date.new(1900, 1, 1)
+cur_date.instance_variable_set('@dayofweek', 2)
+
+total = 0
+until cur_date.year == 2001
+  if cur_date.year < 1901
+    cur_date.next_day
+  else
+    total += 1 if cur_date.day == 1 && cur_date.dayofweek == 1
+    cur_date.next_day
+  end
+end
+
+puts total
--- 19/test_euler_19.rb
+++ 19/test_euler_19.rb
@@ -4,15 +4,72 @@ require 'test/unit'
 # Test euler_19
 class TestEuler19 < Test::Unit::TestCase
   def test_leap_year
-    assertions = [
+    test_years = [
       [0, true],
       [1, false],
       [4, true],
       [100, false],
       [400, true]
     ]
-    assertions.each do |assertion|
-      assert_equal(assertion[1], leap_year?(assertion[0]))
+    test_years.each do |test_year|
+      assert_equal(test_year[1], leap_year?(test_year[0]))
+    end
+  end
+
+  def test_date_initialize
+    test_date = Date.new(1901, 3, 1, 1)
+    assert_equal(1901, test_date.year)
+    assert_equal(3, test_date.month)
+    assert_equal(1, test_date.day)
+    assert_equal(1, test_date.dayofweek)
+  end
+
+  def test_next_dayofweek
+    test_date = Date.new(1901, 1, 1, 1)
+  end
+
+  def test_next_day
+    good_dates = [
+      [Date.new(1901, 1, 1), 1901, 1, 2],
+      [Date.new(1901, 1, 31), 1901, 2, 1],
+      [Date.new(1901, 2, 28), 1901, 3, 1],
+      [Date.new(2000, 2, 29), 2000, 3, 1],
+      [Date.new(1901, 3, 31), 1901, 4, 1],
+      [Date.new(1901, 4, 30), 1901, 5, 1],
+      [Date.new(1901, 5, 31), 1901, 6, 1],
+      [Date.new(1901, 6, 30), 1901, 7, 1],
+      [Date.new(1901, 7, 31), 1901, 8, 1],
+      [Date.new(1901, 8, 31), 1901, 9, 1],
+      [Date.new(1901, 9, 30), 1901, 10, 1],
+      [Date.new(1901, 10, 31), 1901, 11, 1],
+      [Date.new(1901, 11, 30), 1901, 12, 1],
+      [Date.new(1901, 12, 31), 1902, 1, 1]
+    ]
+    bad_dates = [
+      [Date.new(1901, 1, 31), 1901, 1, 32],
+      [Date.new(1901, 2, 28), 1901, 2, 29],
+      [Date.new(2000, 2, 29), 2000, 2, 30],
+      [Date.new(1901, 3, 31), 1901, 3, 32],
+      [Date.new(1901, 4, 30), 1901, 4, 31],
+      [Date.new(1901, 5, 31), 1901, 5, 32],
+      [Date.new(1901, 6, 30), 1901, 6, 31],
+      [Date.new(1901, 7, 31), 1901, 7, 32],
+      [Date.new(1901, 8, 31), 1901, 8, 32],
+      [Date.new(1901, 9, 30), 1901, 9, 31],
+      [Date.new(1901, 10, 31), 1901, 10, 32],
+      [Date.new(1901, 11, 30), 1901, 11, 31],
+      [Date.new(1901, 12, 31), 1901, 12, 32]
+    ]
+    good_dates.each do |good_date|
+      good_date[0].next_day
+      assert_equal(good_date[1], good_date[0].year)
+      assert_equal(good_date[2], good_date[0].month)
+      assert_equal(good_date[3], good_date[0].day)
+    end
+    bad_dates.each do |bad_date|
+      bad_date[0].next_day
+      assert_not_equal(bad_date[2], bad_date[0].month)
+      assert_not_equal(bad_date[3], bad_date[0].day)
     end
   end
 end
